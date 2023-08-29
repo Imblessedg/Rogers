d5c4914c3c1d1a4b41726ee279fcdf911949989d
Imblessedg <143499461+Imblessedg@users.noreply.github.com>
2023-08-29 03:01:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_zlpml8k", "template_an25pvr", formData)
+    emailjs.send(service_zlpml8k, "template_an25pvr", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
